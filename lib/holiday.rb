cc23e319a6876a075a4c803ddebb2245d1587fd0
conner fortner <fortner.conner@yahoo.com>
2020-09-19 02:52:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -45,14 +45,17 @@ def all_winter_holiday_supplies(holiday_hash)
 end
   array.flatten
 def all_supplies_in_holidays(holiday_hash)
-  
+   holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+     end
+   end
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  
 end
 
 
