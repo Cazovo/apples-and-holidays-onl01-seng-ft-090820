d156348e9ec1511f8e8c57c7f63410c5ad85daa8
conner fortner <fortner.conner@yahoo.com>
2020-09-19 02:59:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,7 @@ def all_winter_holiday_supplies(holiday_hash)
     
 end
   array.flatten
-  
+ end 
 def all_supplies_in_holidays(holiday_hash)
    holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
