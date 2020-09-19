0ee1c2cc9f08d03973d761468a9760e08aca000c
conner fortner <fortner.conner@yahoo.com>
2020-09-19 03:52:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,8 +41,14 @@ end
   
  
 def all_supplies_in_holidays(holiday_hash)
-  
-
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+end
+  end
+  end
+    
 def all_holidays_with_bbq(holiday_hash)
   answer = []
 holiday_hash.each do |season, data|
